0b8eb5880413e8760df186b7e1ddc55a63878a15
Silje Marie Torheim <siljemarietorheim@Elkjps-MacBook-Pro-3.local>
2019-03-14 09:36:21 +0100

    Oppdatering

--- js/nim.js
+++ js/nim.js
@@ -1,5 +1,3 @@
-//Her skal dere definere en konstruktør som oppretter et objekt som representerer et nim-spill.
-
 /*class Player {
   constructor(name, human) {
     this.name = name;
@@ -41,7 +39,7 @@ function Nimm(player_1, player_2, victory, total, maxGrab) {
     }
   }
 
-  if (player_1 != undefined) { //oppretter en spiller om player_1 parameteret ikke er tomt. player_1 parameteret blir brukt til å navngi spilleren.
+  if (player_1 != undefined) {
     this.player_1 = {
       name: player_1,
       human: true
@@ -50,33 +48,20 @@ function Nimm(player_1, player_2, victory, total, maxGrab) {
 
 
 }
-function writeToPage(id, param){ //funksjon som tar to parametre: id og param. id er en id til et element fra html siden, param er det du ønsker å skrive til HTML siden.
+function writeToPage(id, param){
   var data = document.getElementById(id);
   data.innerHTML = param
 }
 
 
 function program(){
-  var spill = new Nimm("Ola") // oppretter et spill objekt.
-  writeToPage("total2", spill.total); // kjører funksjonen som henter et HTML element og skriver noe til det.
-  //henter ut <p> taggen med id = "total". Setter inn spill.total (som er total til spill objektet vi definerte to linjer opp), som vi vet = 14.
-
+  var spill = new Nimm("Ola")
+  writeToPage("total2", spill.total);
 }
 
 function k(spiller, nummer){
   console.log(spiller, nummer);
-  
+  while true {
+    new total = total - nummer;
+  }
 }
-
-
-/*
-//Knappene
-
-function k(spiller, nummer){
-  console.log(spiller, nummer);
-  count = total - number
-}
-
-for (var count in k) {
-  if ()
-}*/
